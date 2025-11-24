a81ade86f8710756b5730ae1c64ab7d030ce4a21
Carlos-E-Souza <carlos.e.souza2653@gmail.com>
2025-11-24 20:24:19 -0300

    Add views, procedures, and triggers

--- gerarTabelas.sql
+++ gerarTabelas.sql
@@ -143,3 +143,86 @@ CREATE TABLE AVALIACAO (
     FOREIGN KEY (cod_morador) REFERENCES MORADOR(cod_morador)
         ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
+-- VIEWS
+
+CREATE VIEW vw_avaliacoes_completas AS
+SELECT
+    a.cod_aval,
+    s.cod_servico,
+    s.nome          AS servico_nome,
+    s.descr         AS servico_descr,
+    o.cod_oco,
+    o.tipo_status,
+    org.nome        AS orgao_nome,
+    m.cpf           AS morador_cpf,
+    m.nome          AS morador_nome,
+    a.nota_serv,
+    a.nota_tempo,
+    a.opiniao,
+    o.data          AS data_ocorrencia,
+    s.inicio_servico,
+    s.fim_servico
+FROM AVALIACAO a
+JOIN SERVICO s      ON s.cod_servico = a.cod_servico
+JOIN OCORRENCIA o   ON o.cod_oco     = a.cod_ocorrencia
+JOIN ORGAO_PUBLICO org ON org.cod_orgao = s.cod_orgao
+JOIN MORADOR m      ON m.cpf          = a.cod_morador;
+
+
+-- PROCEDURES
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_registrar_avaliacao (
+    IN p_cod_ocorrencia INT,
+    IN p_cod_servico    INT,
+    IN p_cod_morador    VARCHAR(11),
+    IN p_nota_serv      INT,
+    IN p_nota_tempo     INT,
+    IN p_opiniao        VARCHAR(200),
+    OUT p_novo_cod_aval INT
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        SET p_novo_cod_aval = NULL;
+    END;
+
+    START TRANSACTION;
+
+    INSERT INTO AVALIACAO (cod_ocorrencia, cod_servico, cod_morador, nota_serv, nota_tempo, opiniao)
+    VALUES (p_cod_ocorrencia, p_cod_servico, p_cod_morador, p_nota_serv, p_nota_tempo, p_opiniao);
+
+    SET p_novo_cod_aval = LAST_INSERT_ID();
+
+    UPDATE OCORRENCIA
+    SET tipo_status = 'AVALIADA'
+    WHERE cod_oco = p_cod_ocorrencia;
+
+    COMMIT;
+END $$
+
+DELIMITER ;
+
+
+-- TRIGGERS
+
+DELIMITER $$
+
+CREATE TRIGGER trg_avaliacao_media_after_insert
+AFTER INSERT ON AVALIACAO
+FOR EACH ROW
+BEGIN
+    UPDATE SERVICO s
+    SET s.nota_media_servico = (
+        SELECT AVG((a.nota_serv + a.nota_tempo) / 2.0)
+        FROM AVALIACAO a
+        WHERE a.cod_servico = NEW.cod_servico
+    )
+    WHERE s.cod_servico = NEW.cod_servico;
+END $$
+
+DELIMITER ;
